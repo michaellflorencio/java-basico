8f654a2cab669c7439e1c29b17d5441fd1a1e14a
michaellflorencio <michael.lima.florencio@gmail.com>
2024-06-11 23:58:45 -0300

    m1

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main(String[] args) {
+       int numero1 = 1;
+       int numero2 = 2;
+       boolean resultado = numero1 == numero2;
+       System.out.println(resultado);
+       
+    }
+}
